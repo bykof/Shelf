339052dd21ba25179bc2cc5f20ac973ac157c5dd
Michael Bykovski <bykof@me.com>
2015-03-29 20:29:50 +0200

    Paar neue Sprüche geschrieben

--- site/js/angular/services.js
+++ site/js/angular/services.js
@@ -2,7 +2,13 @@ var module = angular.module("shelfModule");
 var loaderTexts = [
     "Wasche die Hardware mit Seife...",
     "Suche Rechnungen in der Mülltone...",
-    "Mache Yoga..."
+    "Mache Yoga...",
+    "Versuche die Maus zu fangen...",
+    "Suche ANY KEY auf der Tastatur...",
+    "Fixe schnell ein paar Bugs...",
+    "Suche schnell die Codezeilen...",
+    "Versuche mich mit 127.0.0.1 zu verbinden...",
+    "Gehe kurz schlafen...",
 ];
 
 module.service("loaderTexts", function() {
